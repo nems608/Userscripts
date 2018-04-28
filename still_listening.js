6e800815c1fc2633cf1a3011ee4a092095af009b
Nic Mews-Schmuck <nems608@gmail.com>
2018-04-27 22:25:50 -0500

    Pandora: Still Listening

---
+++ still_listening.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Pandora Still Listening
+// @namespace    https://github.com/nems608
+// @version      0.1
+// @description  Automatically clickc the 'Still Listening' button on Pandora
+// @author       Nic Mews-Schmuck
+// @match        https://www.pandora.com/
+// @grant        none
+// ==/UserScript==
+
+function stillListening() {
+  var popup = document.getElementsByClassName('StillListeningBody')[0];
+  var btn = popup.getElementsByTagName('button')[0];
+  btn.click();
+}
+
+setInterval(stillListening, 1000);
