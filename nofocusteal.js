0c6999d85d308f0fbf561e8bcecc4091542782b4
Nic Mews-Schmuck <nems608@gmail.com>
2018-10-18 21:20:54 -0500

    Attempt to prevent vlcoud from stealing focus when a console reconnects

---
+++ nofocusteal.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name       vmware don't steal focus
+// @namespace  http://github.com/nems608/userscripts
+// @version    0.1
+// @description  no
+// @match      https://vcloud.ialab.dsu.edu/cloud/WebMKSConsole.html
+// ==/UserScript==
+
+window.focus = null;
